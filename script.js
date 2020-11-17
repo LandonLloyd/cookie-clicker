c7ba0475e08f75e37143090599196c6663bfbb48
Landon Lloyd <65516863+LandonLloyd@users.noreply.github.com>
2020-11-16 20:12:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let num = document.getElementById("num");
 let acPrice = document.getElementById("ac-price");
-let autoClickPrice = 50;
+let acCountp = document.getElementById("ac-count");
+let acCount = 0;
+let autoClickPrice = 100;
 
 function scoreUp(){
   num.innerText++;
@@ -11,6 +13,9 @@ function auto(){
     num.innerText = num.innerText - autoClickPrice;
     autoClickPrice = autoClickPrice + 25;
     acPrice.innerText = "Price: " + autoClickPrice + " Cookies";
+    acCount++;
+    acCountp.innerText = "You have " + acCount + " autoclickers";
+    time = time + 250;
     setInterval(function(){
       num.innerText++;
     }, 1000);
